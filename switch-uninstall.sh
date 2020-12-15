0246ef0102051829a09a16791ade132e7de87929
Christos Kotsis <kotsis.chr@gmail.com>
2020-12-15 23:08:16 +0200

    Remove recursive flag from rm

--- switch-uninstall.sh
+++ switch-uninstall.sh
@@ -9,27 +9,27 @@ fi
 
 echo 'Removing optimus-switch'
 
-rm -rf /usr/local/bin/set-amd.sh
-rm -rf /usr/local/bin/set-nvidia.sh
-rm -rf /usr/local/bin/optimus.sh
-rm -rf /usr/share/sddm/scripts/Xsetup
-rm -rf /etc/switch
+rm -f /usr/local/bin/set-amd.sh
+rm -f /usr/local/bin/set-nvidia.sh
+rm -f /usr/local/bin/optimus.sh
+rm -f /usr/share/sddm/scripts/Xsetup
+rm -f /etc/switch
 
 systemctl disable disable-nvidia.service
-rm -rf /etc/systemd/system/disable-nvidia.service
+rm -f /etc/systemd/system/disable-nvidia.service
 
-rm -rf /etc/X11/mhwd.d/99-nvidia.conf
-rm -rf /etc/X11/xorg.conf.d/99-nvidia.conf
-rm -rf /etc/modprobe.d/99-nvidia.conf
-rm -rf /etc/modules-load.d/99-nvidia.conf
+rm -f /etc/X11/mhwd.d/99-nvidia.conf
+rm -f /etc/X11/xorg.conf.d/99-nvidia.conf
+rm -f /etc/modprobe.d/99-nvidia.conf
+rm -f /etc/modules-load.d/99-nvidia.conf
 
-rm -rf /etc/X11/xorg.conf.d/99-amd.conf
-rm -rf /etc/modprobe.d/99-amd.conf
-rm -rf /etc/modules-load.d/99-amd.conf
+rm -f /etc/X11/xorg.conf.d/99-amd.conf
+rm -f /etc/modprobe.d/99-amd.conf
+rm -f /etc/modules-load.d/99-amd.conf
 
 sleep 1
 echo 'optimus-switch is now uninstalled'
 echo '########'
 echo 'if you reboot now, you will not have a working Xorg setup.'
 echo 'setup another optimus solution before rebooting.'
-rm -rf uninstall-switch.sh 
+rm -f uninstall-switch.sh 
--- switch/set-amd.sh
+++ switch/set-amd.sh
@@ -5,10 +5,10 @@
 #with modeset.xorg.conf on the line below.
 echo 'Removing nvidia prime setup......'
 
-rm -rf /etc/X11/xorg.conf.d/99-nvidia.conf
-rm -rf /etc/modprobe.d/99-nvidia.conf
-rm -rf /etc/modules-load.d/99-nvidia.conf
-rm -rf /usr/share/sddm/scripts/Xsetup
+rm -f /etc/X11/xorg.conf.d/99-nvidia.conf
+rm -f /etc/modprobe.d/99-nvidia.conf
+rm -f /etc/modules-load.d/99-nvidia.conf
+rm -f /usr/share/sddm/scripts/Xsetup
 
 sleep 1
 echo 'Setting amd only mode.......'
